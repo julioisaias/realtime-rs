b8197bd069344bc79851b275f9cd8f283a790fac
Julio <julioisaias@gmail.com>
2021-09-02 18:12:34 +0000

    Severals

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use crate::supabase::Client;
 
 fn main() {
 
-    let url = "wss://qfdzxqspsbaiqhrehiia.supabase.co";
-    let api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMTc0MjYwMywiZXhwIjoxOTM3MzE4NjAzfQ.zZaAHGQPi1hZ4nJy5CofZRIP686x_8j5_YzFYwEdWNk";
+    let url = "wss://abc.supabase.co";
+    let api = "zZaAHGQPi1hZ4nJy5CofZRIP686x_8j5_YzFYwEdWNk";
 
 
     let client = Client::new(url);
